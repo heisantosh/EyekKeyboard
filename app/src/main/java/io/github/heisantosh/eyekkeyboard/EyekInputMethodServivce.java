9d60c71dca9e64a91ed9bcf8f122bf4707a3c334
Santosh Heigrujam <santosh.hei@gmail.com>
2018-12-28 09:59:35 +0530

    Update keycodes for activating custom layouts

--- app/src/main/java/io/github/heisantosh/eyekkeyboard/EyekInputMethodServivce.java
+++ app/src/main/java/io/github/heisantosh/eyekkeyboard/EyekInputMethodServivce.java
@@ -5,6 +5,7 @@ import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
 
@@ -52,17 +53,18 @@ public class EyekInputMethodServivce extends InputMethodService implements Keybo
                 }
                 ic.deleteSurroundingText(1, 0);
                 break;
-            case -1:
-                mKeyboardView.setKeyboard(mAmaAniAhumKeyboard);
-                Log.d(TAG, "onKey: -1 : setting ama ani ahum keyboard");
-                break;
-            case -2:
-                mKeyboardView.setKeyboard(mKokSamLaiKeyboard);
-                Log.d(TAG, "onKey: -2 : setting kok sam lai keyboard");
-                break;
             case -4:
+                getCurrentInputConnection().sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                getCurrentInputConnection().sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_ENTER));
+                break;
+            case -10002:
+                mKeyboardView.setKeyboard(mAmaAniAhumKeyboard);
+                break;
+            case -10001:
+                mKeyboardView.setKeyboard(mKokSamLaiKeyboard);
+                break;
+            case -10003:
                 mKeyboardView.setKeyboard(mExtensionsKeyboard);
-                Log.d(TAG, "onKey: -4 : setting mayek extensions keyboard");
                 break;
             default:
                 char code = (char) primaryCode;
@@ -72,7 +74,8 @@ public class EyekInputMethodServivce extends InputMethodService implements Keybo
 
     @Override
     public void onPress(int primaryCode) {
-        if (primaryCode > 0) {
+        // Hide key preview for DEL, SPACE, ENTER and layout switching keys.
+        if (primaryCode > 0 && primaryCode != 32) {
             mKeyboardView.setPreviewEnabled(true);
         }
         else {
